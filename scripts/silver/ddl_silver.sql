8f2473adaa270e840bd86ceb8f45b50e9930a2b4
David Northey <dnorthey88@gmail.com>
2026-01-10 21:47:42 +1100

    Update ddl_silver.sql to specify changes to the DDL

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -14,7 +14,6 @@ IF OBJECT_ID ('silver.brands', 'U') IS NOT NULL
 CREATE TABLE silver.brands (
 	product_id NVARCHAR(50),
 	brand NVARCHAR(50),
-	modified_brand NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -22,25 +21,19 @@ IF OBJECT_ID ('silver.finance', 'U') IS NOT NULL
 	DROP TABLE silver.finance;
 CREATE TABLE silver.finance (
 	product_id NVARCHAR(50),
-	listing_price NVARCHAR(50),
-	sale_price NVARCHAR(50),
-	discount NVARCHAR(50),
-	revenue NVARCHAR(50),
-	modified_listing_price NVARCHAR(50),
-	modified_sale_price NVARCHAR(50),
-	modified_discount NVARCHAR(50),
-	modified_revenue NVARCHAR(50),
+	listing_price DECIMAL(10,2),
+	sale_price DECIMAL(10,2),
+	discount DECIMAL(6,4),
+	revenue DECIMAL(10,2),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID ('silver.info', 'U') IS NOT NULL
 	DROP TABLE silver.info;
 CREATE TABLE silver.info (
-	product_name NVARCHAR(MAX),
 	product_id NVARCHAR(50),
+	product_name NVARCHAR(MAX),
 	description NVARCHAR(MAX),
-	modified_product_name NVARCHAR(MAX),
-	modified_description NVARCHAR(MAX),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -48,13 +41,8 @@ IF OBJECT_ID ('silver.reviews', 'U') IS NOT NULL
 	DROP TABLE silver.reviews;
 CREATE TABLE silver.reviews (
 	product_id NVARCHAR(50),
-	rating NVARCHAR(50),
-	reviews NVARCHAR(50),
-	Hour NVARCHAR(50),
-	minute NVARCHAR(50),
-	real_rating NVARCHAR(50),
-	real_reviews NVARCHAR(50),
-	unnamed_7 NVARCHAR(50),
+	rating DECIMAL(3,1),
+	review_count INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -62,7 +50,6 @@ IF OBJECT_ID ('silver.traffic', 'U') IS NOT NULL
 	DROP TABLE silver.traffic;
 CREATE TABLE silver.traffic (
 	product_id NVARCHAR(50),
-	last_visited NVARCHAR(50),
-	modified_last_visited NVARCHAR(50),
+	last_visited DATETIME2,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
